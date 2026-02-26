20e10f593dfde91a394b62641b78e770548037c1
MCbasketball000 <3811737414@qq.com>
2026-02-26 19:25:49 +0800

    commit

--- init.js
+++ init.js
@@ -1,5 +1,5 @@
 document.body.style.backgroundColor = "#282828";
-var newDiv = document.createElement(div);
+var newDiv = document.createElement("div");
 newDiv.textContent = "作者：MC篮球";
 newDiv.style.position = "absolute";
 newDiv.style.bottom = "0px";
