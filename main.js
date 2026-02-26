cb754bb9850531aeadc57e1d30f6ae181bf9fadb
MCbasketball000 <3811737414@qq.com>
2026-02-26 22:57:34 +0800

    1

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function CreateElementBy(type,content,pos,pos1,pos2,opcode,lengthz,widthz){
     newDiv.style.height = lengthz;
     newDiv.textContent = content;
     newDiv.style.position = "absolute";
+    newDiv.id = opcode;
     //pos：1是左上，2是右上，3是左下，4是右下
     if(pos == 1)
     {
@@ -25,14 +26,17 @@ function CreateElementBy(type,content,pos,pos1,pos2,opcode,lengthz,widthz){
         newDiv.style.bottom = pos1;
         newDiv.style.right = pos2;
     }
-    newDiv.addEventListener("mouseenter", function(){
-        this.classList.add('highlight');
-    });
-    newDiv.addEventListener("mouseleave", function(){
-        this.classList.remove('highlight');
-    });
-    newDiv.addEventListener("click", use(opcode));
-    document.body.appendChild(newDiv);
+    if(type == "button")
+    {
+        newDiv.addEventListener("mouseenter", function(){
+            this.classList.add('highlight');
+        });
+        newDiv.addEventListener("mouseleave", function(){
+            this.classList.remove('highlight');
+        });
+        newDiv.addEventListener("click", use(opcode));
+        document.body.appendChild(newDiv);
+    }
 }
 function use(opcode){
     ;
