309d596b5c25a8d17834b53eb294d9fa8c5df0d8
MCbasketball000 <3811737414@qq.com>
2026-02-26 19:21:52 +0800

    1

--- init.js
+++ init.js
@@ -4,3 +4,7 @@ newDiv.textContent = "作者：MC篮球";
 newDiv.style.position = "absolute";
 newDiv.style.bottom = "0px";
 newDiv.style.right = "0px";
+newDiv.addEventListener("click", function(){
+    window.open('https://space.bilibili.com/3546613752531863?spm_id_from=333.1369.0.0', '_blank');
+});
+document.body.appendChild(newDiv);
